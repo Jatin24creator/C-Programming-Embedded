924c55451c70808c865f1be2b7b718f6af2da1bd
jatin24creator <jatin24ranas@gmail.com>
2026-03-02 23:54:31 +0530

    Solved 5 QUESTIONS

--- level4/problem20.c
+++ level4/problem20.c
@@ -1,25 +1,27 @@
 /*Question:  Write a program to print the total number of  single digit Prime numbers 
 Assume 0 & 1 are not Prime. 
 Answer : 4 */
-#include<Stdio.h>
-int main (){
-    int count = 0,x =0;
-    for (int i = 1; i <10; i++){ // run for each number
-        x =0;
-        for (int j = 1; j <i; j++) // check if divisible by any number
-        {
-            if(i%j == 0 && j !=1){ // if divisibile and number is not 1
-                x =0;
+#include <stdio.h>
+
+int main() {
+    int count = 0;
+
+    for (int i = 2; i < 10; i++) {  // Start from 2
+        int isPrime = 1;           // Assume prime
+
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {      // If divisible
+                isPrime = 0;
                 break;
-            } else if ( j!=1 && j!=0) { // not divisible and number is 1
-                x =1; //prime
             }
         }
-       
-        if(x==1){
-             printf("%d\n",i);
+
+        if (isPrime) {
+            printf("%d\n", i);
             count++;
         }
     }
-    printf("%d",count);
+
+    printf("Total prime numbers: %d\n", count);
+    return 0;
 }
