ccc33539430153c20299cc986cdf6ca5f7b52ee9
Jatin Rana <jatin24ranas@gmail.com>
2026-03-09 16:08:03 +0530

    fixed the loop issue

--- level4/problem32.c
+++ level4/problem32.c
@@ -6,27 +6,28 @@ Example: 59.  5 + 9 = 14 */
 
 #include<Stdio.h>
 int main(){
-    int start = 10,count = 0,sum =0;
-    while(start <1000000){ // while the number is less than the limit set
-        int i = start; // set the temp variable for another while loop
-        //check prime
-        int isPrime =1;
-        for (int j = 2; j<i; j++){
-            if(i%j ==0){
-                isPrime =0;
+    // Total number of prime numbers below 1,000,000 wih sum ==14
+    int sum =0,count =0;
+    for (int i =10 ; i <= 1000000; i++){ // runs from 10 to 1,000,000
+        int isPrime=1,temp =i; // set the isPrime flag as true
+        for (int j = 2; j<i;j++){ // for loop of numbers less than the number
+            if(i%j ==0){ // check if divisible
+                isPrime =0; // setFlag to not prime
                 break;
             }
         }
-        if(isPrime ==1){ // if prime
-            while(i>0){ // calculate the sum of digits
-                sum = sum +i%10;
-                i = i/10;
+        
+        if(isPrime ==1){ // if Prime
+             //printf("%d\n is prime",i);
+            while(temp>0){
+                sum = sum +temp%10;
+                temp = temp/10;
             }
-            if(sum == 14){
+            if(sum ==14){
                 count++;
-                        }
+            }
         }
-        start++;
+        sum =0; // reset sum =0
     }
     printf("%d",count);
 }
