99dc1551bf00f5cd85a7d4d5b421e286129b3080
Jatin Rana <jatin24ranas@gmail.com>
2026-03-13 23:43:18 +0530

    problems

---
+++ level4/problem38.c
@@ -0,0 +1,9 @@
+/*Question:  Get a String and print the same. */
+#include<Stdio.h>
+int main(){
+    char str[100];
+    printf("Enter the string: ");
+    scanf("%s",str);
+    printf("%s",str);
+
+}
