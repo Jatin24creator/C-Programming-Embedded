d02531caa2df88605229c5e03b8abc2fbaa12392
jatin24creator <jatin24ranas@gmail.com>
2026-07-01 00:36:09 +0530

    test inclusive of the functions

--- Projects-Learning/BitLab/test/test.c
+++ Projects-Learning/BitLab/test/test.c
@@ -44,13 +44,76 @@ int n;
 }*/
 /**/
 #include<stdio.h>
+#include<stdint.h>
 
-int main (){
-    int n;
-    printf("Enter the number: ");
-    scanf("%d",&n);
+void dec2bin (int n){
+    
     for(int i =7; i>=0;i--){
         printf("%d",(n>>i)&1);
     }
+    printf("\n");
+}
+void binAnd(int n1, int n2){
+    dec2bin(n1);
+    dec2bin(n2);
+    int result = n1&n2;
+    dec2bin(result);
+    printf("%d",result);
+}
+void binOR(int n1,int n2){
+    dec2bin(n1);
+    dec2bin(n2);
+    int result =n1|n2;
+    printf("%d",result);
+}
+
+void binXOR(int n1, int n2){
+    dec2bin(n1);
+    dec2bin(n2);
+    int result =n1^n2;
+    printf("%d",result);
+}
+void binNOT(uint8_t n1){
+    printf("0x%hhx\n",~n1);
+
+}
+
+void main(){
+    uint8_t n1,n2;
+    int choice;
+    printf("Enter the operation:\n"
+        "1 for OR\n"
+        "2 for AND\n"
+        "3 for XOR\n"
+        "4 for NOT\n");
+    scanf("%d",&choice);
+   if(choice ==4){
+    printf("Enter the number in HEX 0x..: ");
+    scanf("%hhx",&n1);
+   }else{
+    printf("Enter the number 1: ");
+    scanf("%d",&n1);
+    printf("Enter the number 2: ");
+    scanf("%d",&n2);
+   }
+
+
+    switch(choice){
+        case 1:
+        binOR(n1,n2); 
+        break;
+        case 2:
+        binAnd(n1,n2);
+        break;
+        case 3: 
+        binXOR(n1,n2);
+        break;
+        case 4:
+        binNOT(n1);
+        break;
+        default:
+        printf("Enter correct option\n");
+    }  
+
 }
     
